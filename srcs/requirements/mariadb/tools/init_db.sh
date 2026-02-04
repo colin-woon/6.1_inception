0c7a5296b0e244df0a148e56a2583dc03570b017
Colin Woon <colin.wyj@gmail.com>
2026-02-04 08:14:27 +0000

    feat:
    - isolate db initialization & security configuration for true idempotency

--- srcs/requirements/mariadb/tools/init_db.sh
+++ srcs/requirements/mariadb/tools/init_db.sh
@@ -27,17 +27,21 @@ check_secret "$MYSQL_USER" "MYSQL_USER"
 check_secret "$MYSQL_PASSWORD" "MYSQL_PASSWORD"
 check_secret "$MYSQL_ROOT_PASSWORD" "MYSQL_ROOT_PASSWORD"
 
-# 1. Idempotency check
-if [ -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
-	echo "Database ${MYSQL_DATABASE} already initialized. Starting server..."
-	exec mariadbd --user=mysql --datadir=/var/lib/mysql --bind-address=0.0.0.0
+# Check if the database variables are actually set (Safety Check)
+if [ -z "$MYSQL_DATABASE" ]; then
+    echo "Error: MYSQL_DATABASE is not set!"
+    exit 1
 fi
 
-echo "Initializing Database ${MYSQL_DATABASE}"
+# 1. Idempotency check
+if [ ! -d "/var/lib/mysql/${MYSQL_DATABASE}" ]; then
+	echo "Initializing Database ${MYSQL_DATABASE}"
+	mariadb-install-db --user=mysql --datadir=/var/lib/mysql
+fi
 
 # 2. Start temporary server
-mariadb-install-db --user=mysql --datadir=/var/lib/mysql
 # & puts mariadb in background, $! gets the most recently executed process ID
+echo "Database ${MYSQL_DATABASE} already initialized. Starting server..."
 mariadbd --user=mysql --datadir=/var/lib/mysql --skip-networking & PID="$!"
 
 # 3. Polling
@@ -47,16 +51,7 @@ until mysqladmin ping -h localhost --silent; do
 	sleep 1
 done
 
-# 4. Check if the database variables are actually set (Safety Check)
-if [ -z "$MYSQL_DATABASE" ]; then
-    echo "Error: MYSQL_DATABASE is not set!"
-    exit 1
-fi
-
-# 5. The SQL Injection (Now using variables!)
-# -- Create Remote Root (The Missing Piece)
-# CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY '${MYSQL_ROOT_PASSWORD}';
-# GRANT ALL PRIVILEGES ON *.* TO 'root'@'%' WITH GRANT OPTION;
+# 4. The SQL Injection (Now using variables!)
 mariadb -u root <<_EOF_
 -- Fix Local Root
 -- We use ALTER here because root@localhost is created by install-db without a password (socket auth)
@@ -73,11 +68,11 @@ GRANT SELECT, INSERT, UPDATE, DELETE, CREATE, DROP, ALTER, INDEX ON `${MYSQL_KUM
 FLUSH PRIVILEGES;
 _EOF_
 
-# 6. CLeanly shutdown temporary server
+# 5. CLeanly shutdown temporary server
 echo "Shutting down temporary server"
 mysqladmin -u root -p"${MYSQL_ROOT_PASSWORD}" shutdown
 wait "$PID" # script will not execute next if previous PID was not shutdown properly
 
-# 7. Start MariaDB as PID 1
+# 6. Start MariaDB as PID 1
 echo "Replacing MariaDB as PID 1"
 exec mariadbd --user=mysql --bind-address=0.0.0.0
